971110c5e239a95307bc5cd8c459c11167a8b333
Josh Malkinson <josh.malkinson@gmail.com>
2015-07-07 13:30:36 +0100

    Express app to perform merge of two objects

---
+++ server.js
@@ -0,0 +1,20 @@
+/*jshint strict:false*/
+/*jshint unused:false*/
+/*globals require*/
+
+var express = require('express');
+var _ = require('lodash');
+
+var app = express();
+
+var machineConfig = { 'I Am a' : 'Machine' };
+var humanConfig = { 'I Am a' : 'Human', 'honest': true };
+
+app.get('/', function(req, res) {
+    var mergeResult = _.merge(machineConfig, humanConfig);
+    res.json(mergeResult);
+});
+
+var server = app.listen(3000, function () {
+    console.log('Listening on localhost:3000');
+});
